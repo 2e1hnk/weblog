13f738c12717cd0b7c63566dcf3e983c5754bb4b
2E1HNK <2e1hnk@deadbeef.cafe>
2019-07-28 20:54:25 +0100

    Added internationalisation support and English+French language files

--- weblog/src/main/java/weblog/Application.java
+++ weblog/src/main/java/weblog/Application.java
@@ -1,10 +1,15 @@
 package weblog;
 
+import java.util.Locale;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @SpringBootApplication
 @EnableConfigurationProperties(StorageProperties.class)
---
+++ weblog/src/main/java/weblog/WebConfig.java
@@ -0,0 +1,5 @@
+package weblog;
+
+public class WebConfig {
+
+}
