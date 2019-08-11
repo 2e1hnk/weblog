069fa5469ea339ae4ac286e8d2bc620627b514ff
2E1HNK <2e1hnk@deadbeef.cafe>
2019-08-11 21:51:46 +0100

    Removing Security stuff

--- weblog/src/main/java/weblog/Application.java
+++ weblog/src/main/java/weblog/Application.java
@@ -1,16 +1,9 @@
 package weblog;
 
-import java.util.Locale;
-
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.servlet.LocaleResolver;
-import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
-import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @SpringBootApplication
 @EnableConfigurationProperties(StorageProperties.class)
@@ -20,14 +13,4 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-    
-    /*
-    @Bean
-    CommandLineRunner init(StorageService storageService) {
-        return (args) -> {
-            storageService.deleteAll();
-            storageService.init();
-        };
-    }
-    */
 }
--- weblog/src/main/java/weblog/IUserService.java
+++
@@ -1,5 +0,0 @@
-package weblog;
-
-public interface IUserService {
-    User registerNewUserAccount(UserDto accountDto) throws EmailExistsException;
-}
