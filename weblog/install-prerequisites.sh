be8f55bed5a5ff05205f47f46df3aa94ae2852d1
2E1HNK <2e1hnk@deadbeef.cafe>
2019-09-16 16:56:05 +0100

    update

--- weblog/install-prerequisites.sh
+++ weblog/install-prerequisites.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 sudo apt-get install mysql-server openjdk-8-jre openjdk-8-jdk-headless screen gradle nginx
-mysqladmin --user=root password "password"
-mysql -u root -p -e "CREATE DATABASE weblog;"
-mysql -u root -p -e "CREATE USER 'weblog'@'localhost' IDENTIFIED BY 'password';"
-mysql -u root -p -e "GRANT ALL PRIVILEGES ON weblog.* TO 'weblog'@'localhost';"
+sudo mysqladmin --user=root password "password"
+sudo mysql -u root -p -e "CREATE DATABASE weblog;"
+sudo mysql -u root -p -e "CREATE USER 'weblog'@'localhost' IDENTIFIED BY 'password';"
+sudo mysql -u root -p -e "GRANT ALL PRIVILEGES ON weblog.* TO 'weblog'@'localhost';"
 
 echo "Remember to set the following environment variables:"
 echo "  DB_HOST, DB_PORT, DB_USER, DB_PASS, DB_NAME"
