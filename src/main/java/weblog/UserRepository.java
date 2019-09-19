00a2b9f77b28d6286c6a21ec6dcbce8693840b22
2E1HNK <2e1hnk@deadbeef.cafe>
2019-09-19 15:11:30 +0100

    Fixed @Repository annotation

--- src/main/java/weblog/UserRepository.java
+++ src/main/java/weblog/UserRepository.java
@@ -1,9 +1,11 @@
 package weblog;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import weblog.model.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 	User findByUsername(String username);
 }
