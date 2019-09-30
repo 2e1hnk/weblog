532d2d7536de50e5a0b9b2c98d07036f7cdea162
2E1HNK <2e1hnk@deadbeef.cafe>
2019-09-30 22:37:30 +0100

    Added grid to locations view

--- src/main/resources/create-views.sql
+++ src/main/resources/create-views.sql
@@ -1,20 +1,30 @@
 DROP TABLE IF EXISTS `map`;
 CREATE OR REPLACE VIEW `map` AS
-	SELECT
+	SELECT 
 		`contact`.`id` AS `id`,
-        `contact`.`timestamp` AS `timestamp`,
-		`contact`.`callsign` AS `callsign`,
-		`contact`.`band` AS `band`,
-        CONCAT(`callbook_entry`.`fname`, " ", `callbook_entry`.`name`) AS `name`,
-		`callbook_entry`.`lat` AS `lat`,
-		`callbook_entry`.`lon` AS `lon`
-	FROM (
-		`contact` LEFT JOIN `callbook_entry` ON (
-			(`contact`.`callsign` = `callbook_entry`.`callsign`)
+	    `contact`.`timestamp` AS `timestamp`,
+	    `contact`.`callsign` AS `callsign`,
+	    concat(`callbook_entry`.`fname`,' ',`callbook_entry`.`name`) AS `name`,
+	    COALESCE(
+			(SELECT
+				`location` AS `grid`
+			FROM `weblog`.`contact` `subquery_contact`
+	        WHERE
+				`subquery_contact`.`id` = `contact`.`id`
+	            AND
+	            `location` REGEXP "[a-zA-Z]{2}[0-9]{2}[a-zA-Z]{2}"
+			),
+	        `callbook_entry`.`grid`
+		) AS `grid`,
+	    `callbook_entry`.`lat` AS `lat`,
+	    `callbook_entry`.`lon` AS `lon`
+	FROM
+		(`contact` LEFT JOIN `callbook_entry` ON
+			((`contact`.`callsign` = `callbook_entry`.`callsign`))
 		)
-	)
-	WHERE (
-		(`callbook_entry`.`lat` IS NOT NULL)
-		AND
-		(`callbook_entry`.`lon` IS NOT NULL)
+	WHERE
+		(
+			(`callbook_entry`.`lat` IS NOT null)
+	        AND
+			(`callbook_entry`.`lon` IS NOT null)
 	);
