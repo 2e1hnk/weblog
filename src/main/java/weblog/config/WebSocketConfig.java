c5014206cf92393932241d551b77bee04fb69001
2E1HNK <2e1hnk@deadbeef.cafe>
2019-09-30 12:16:58 +0100

    Changed websocket cors setup

--- src/main/java/weblog/config/WebSocketConfig.java
+++ src/main/java/weblog/config/WebSocketConfig.java
@@ -17,9 +17,9 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/event-stream").withSockJS();		// Used to provide a stream of logging events (e.g. new contact logged) 
-        registry.addEndpoint("/rigctl-update").withSockJS();	// Used to send rig updates to weblog (e.g. new frequency, mode etc.)
-        registry.addEndpoint("/dx-spots").withSockJS();			// DX Cluster spots
+        registry.addEndpoint("/event-stream").setAllowedOrigins("*").withSockJS();		// Used to provide a stream of logging events (e.g. new contact logged) 
+        registry.addEndpoint("/rigctl-update").setAllowedOrigins("*").withSockJS();	// Used to send rig updates to weblog (e.g. new frequency, mode etc.)
+        registry.addEndpoint("/dx-spots").setAllowedOrigins("*").withSockJS();			// DX Cluster spots
     }
 
 }
